a8229224bcd034afd5c31c19393c9067e935378f
Tristan Rechenberger <rechenberger@me.com>
2025-04-08 19:35:48 +0200

    ⚗️ interval

--- src/app/api/abort/route.ts
+++ src/app/api/abort/route.ts
@@ -2,12 +2,20 @@ import { setTimeout } from "timers/promises";
 
 export async function GET(request: Request) {
   console.log("Here we goooooo!");
+
   request.signal.onabort = () => {
     console.log("onabort");
   };
+
   request.signal.addEventListener("abort", () => {
     console.log("abort event");
   });
-  await setTimeout(30_000);
+
+  setInterval(() => {
+    console.log("(interval) aborted:", request.signal.aborted);
+  }, 500);
+
+
+  await setTimeout(5_000);
   return new Response("Hello, world!");
 }
